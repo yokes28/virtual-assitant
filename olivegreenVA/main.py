b9f2b70974dd3bb5b9aa95fa1f1e0749639451ee
yokes28 <76802454+yokes28@users.noreply.github.com>
2021-02-02 15:28:16 +0530

    Update main.py

--- olivegreenVA/main.py
+++ olivegreenVA/main.py
@@ -67,7 +67,7 @@ def location():
     speak('getting ur location as per ur request ')
     speak('your home is at chinniyampalayam')
     speak('opening google maps location for your reference')
-    webbrowser.open("https://www.google.com/maps/place/Chinniyampalayam,+Coimbatore,+Tamil+Nadu/@11.0623152,77.040331,14.5z/data=!4m5!3m4!1s0x3ba85624d5f9b0cf:0x8c0a9bfa4b8ebb40!8m2!3d11.0553431!4d77.0655238")
+    webbrowser.open("enter ur google map location")
 
 def cpu():
     usage = str(psutil.cpu_percent())
@@ -89,7 +89,7 @@ def screenshot():
     img.save(r"F:\screenshots olive\screenshot.png")
 
 def melodies():
-    songs_dir = "E:\yokes\songs\Melodies"
+    songs_dir = "paste ur song location"
     music = os.listdir(songs_dir)
     speak('playing songs randomly')
     speak('enjoy ur time')
@@ -97,21 +97,8 @@ def melodies():
     os.startfile(os.path.join(songs_dir, music[no]))
 
 def spotify():
-    speak('vela pathathu pothum da poi relax pannu')
-    webbrowser.open('https://open.spotify.com/playlist/3DG4JuxAKRLg6zna6Pe04C')
-
-def alarmclock():
-    speak('setting alarm')
-    Set_Alarm = input("enter the time in HH:MM:SS ?")
-    speak('alarmset')
-    Actual_Time = time.strftime("%H:%M:%S")
-    while (Actual_Time == Set_Alarm):
-        speak('wake up buddy , lets move on')
-        playsound("E:\yokes\songs\Melodies\Ethir_Neechal_-_Un_Paarvayil_Video___Sivakarthikeyan%2C_Priya(256k).mp3")
-
-
-
-
+    speak('Get relaxation')
+    webbrowser.open('www.spotify.com')
 
 def websitealarm():
     speak("setting website alarm")
@@ -136,7 +123,7 @@ def weatherreport():
     BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
     speak('enter the city name ?')
     CITY = input('enter the city name ?')
-    API_KEY = "fc59cba909a330b7af27602db31a491a"
+    API_KEY = "enter ur API Key"
     URL = BASE_URL + "q=" + CITY + "&appid=" + API_KEY
     # HTTP request
     response = requests.get(URL)
@@ -161,12 +148,6 @@ def weatherreport():
     else:
         print("Error in the HTTP request")
         speak('error in the HTTP request')
-
-def calculator():
-    speak('opening self build calculator using python ')
-    os.startfile(r"C:\Users\SElangovan\Desktop\notes\main.exe")
-
-
 
 
 def takecommand():
@@ -193,28 +174,7 @@ if __name__=='__main__':
     start()
     speak('i am opening keywords for ur refernce and we shall proceed')
     keywords()
-    buddyday()
-    buddytime()
-    speak('anything else sir ?')
-    speak('please know the CPU usage for ur better performance ')
-    cpu()
-    speak('do you wanna know the weather conditions today')
-    weatherreport()
-    speak('i wish to dedicate a song for you today')
-    melodies()
-
-
-
-
-
-
-
-
-
-
-
-
-
+              
     while True:
         search = takecommand().lower()
 
@@ -241,39 +201,6 @@ if __name__=='__main__':
         elif 'fine' or 'good' or 'ok' in search:
             speak('thats good to hear from you ')
 
-        elif 'task of the day' in search:
-            speak('just  minute it on the way to you ...')
-            note = r'C:\Users\SElangovan\Desktop\shutdown.txt'
-            os.startfile(note)
-            speak('here it is')
-
-        elif 'open arduino' in search:
-            speak("opening arduino IDE")
-            ard = r"C:\Program Files (x86)\Arduino\arduino.exe"
-            os.startfile(ard)
-            speak('her you go ')
-
-        elif 'home location' in search :
-            speak('ur home is at')
-            speak('chinniyampalayam')
-            webbrowser.open("https://www.google.com/maps/place/Chinniyampalayam,+Coimbatore,+Tamil+Nadu/@11.0623152,77.040331,14.5z/data=!4m5!3m4!1s0x3ba85624d5f9b0cf:0x8c0a9bfa4b8ebb40!8m2!3d11.0553431!4d77.0655238")
-
-        elif 'army' in search:
-            speak('opening ur passion of life')
-            webbrowser.open(r"www.joinindianarmy.nic.in")
-
-        elif 'navy' in search:
-            speak('guardians of indianocean')
-            webbrowser.open('www.joinindiannavy.nic.in')
-
-        elif 'airforce' in search:
-            speak('we rule the sky')
-            webbrowser.open("www.joinindianairforce.nic.in")
-
-        elif 'why olivegreen' in search:
-            speak('the reason behind my name is')
-            speak('he is passionate in joining indian armed forces as a resembelence of remembering everytime he used to call me olivegreen ')
-
         elif 'who are you ?' in search:
             speak('i am commanders virtual assistant, created by him')
 
@@ -281,29 +208,8 @@ if __name__=='__main__':
             speak('i will try my evel best to satisfy u')
             speak('what do you what me to do ?')
 
-        elif 'open movies' in search:
-            speak('opening downloaded movies')
-            movie = r"F:\all movies"
-            os.startfile(movie)
-
-        elif 'open video songs' in search:
-            speak('opening songs')
-            song=r"F:\video songs"
-            os.startfile(song)
-
         elif 'jokes' in search:
             speak(pyjokes.get_joke())
-
-        elif 'emergency' in search:
-            speak('displaying ur emergency contacts ?')
-            call=r"C:\Users\SElangovan\Desktop\emergencycontacts.txt"
-            os.startfile(call)
-            speak('be safe')
-
-        elif 'simple calulator' in search:
-            speak('opening simple caculator')
-            cal=r"C:\Users\SElangovan\PycharmProjects\pythoncalculator\dist\main.exe"
-            os.startfile(cal)
 
         elif 'change name' in search:
             speak('what my new name ?')
@@ -342,10 +248,6 @@ if __name__=='__main__':
             speak('opening google maps')
             webbrowser.open("https://www.google.com/maps/search/tourists+places+nearby/")
 
-        elif 'open mail' in search or 'college mail' in search:
-            speak("opening skcet mail")
-            webbrowser.open('https://mail.google.com/mail/u/0/#inbox')
-
         elif 'open meet' in search or 'google meet' in search:
             speak('opening google meet for attending online classes')
             webbrowser.open("https://meet.google.com/")
@@ -379,28 +281,14 @@ if __name__=='__main__':
 
         elif 'search in chrome' in search:
             speak('opening google chrome')
-            cb = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
+            cb = r"paste ur chrome.exe location here"
             speak('what do you want me to search ?')
             search_cb = takecommand().lower()
             webbrowser.get(cb).open_new_tab(search_cb+'.com')
 
         elif 'cpu usage' in search:
             cpu()
-
-        elif 'VM Ware' in search:
-            speak('opening vmware workstation')
-            vm = r"C:\Program Files (x86)\VMware\VMware Workstation\vmware.exe"
-            os.startfile(vm)
-
-        elif 'telegram' in search:
-            speak('opening telegram buddy')
-            tg=r"C:\Windows.old\Users\SElangovan\AppData\Roaming\Telegram Desktop\Telegram.exe"
-            os.startfile(tg)
-
-        elif 'whatsapp' in search:
-            speak('opening whatsapp web')
-            ww = r"C:\Windows.old\Users\SElangovan\AppData\Roaming\Telegram Desktop\Telegram.exe"
-            os.startfile(ww)
+              
 
         elif 'take notes' in search:
             speak('What contents should i take notes sir?')
@@ -425,16 +313,6 @@ if __name__=='__main__':
         elif 'screenshot' in search:
             screenshot()
 
-        elif 'memories' in search:
-            speak('opening tenth memories for you buddy')
-            tenth = r"F:\yokes\coco"
-            os.startfile('tenth')
-
-        elif 'pycharm' in search:
-            speak('opening pycharm')
-            pc = r"C:\Users\SElangovan\Downloads\pycharm-community-2020.3.exe"
-            os.startfile('pc')
-
         elif 'spotify' in search:
             spotify()
 
@@ -448,8 +326,6 @@ if __name__=='__main__':
             speak('bye buddy, have a nice day')
             exit()
 
-        elif 'simple calculator' in search or 'calculator' in search:
-            calculator()
 
 
 
